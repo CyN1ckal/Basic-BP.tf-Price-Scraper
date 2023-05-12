f30a9e49f37eb2435e408158d4a891aed6a1d97e
CyN1ckal <warrior276276276@gmail.com>
2023-05-11 18:22:11 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import statistics
 from time import sleep
@@ -14,7 +16,15 @@ url = "https://next.backpack.tf/classifieds?spell=none&texture=none&quality=11&k
 driver = webdriver.Chrome(executable_path = path)
 
 driver.get(url)
-sleep(2)
+
+
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[3]/div/div/div/div[2]/div/div[3]/div[1]"))
+    )
+except:
+    print("Exception")
 
 selling = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[3]/div/div/div/div[2]/div/div[3]/div[1]")
 buying = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[3]/div/div/div/div[2]/div/div[3]/div[2]")
